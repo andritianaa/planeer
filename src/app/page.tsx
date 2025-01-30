248e9ea59c1cf8a2d5e2b9390949dc862c6ed903
andritianaa <andritianasteve@gmail.com>
2025-01-30 13:10:22 +0300

    add ESLint rule for unused variables and create VSCode snippets for common components and actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 export default function Home() {
-  return <div className="">Hello</div>;
+  return (
+    <div className="space-x-4 p-4">
+      <Input />
+      <Button variant={"primary"}>primary</Button>
+      <Button variant={"secondary"}>secondary</Button>
+      <Button variant={"destructive"}>destructive</Button>
+      <Button variant={"ghost"}>ghost</Button>
+      <Button variant={"link"}>link</Button>
+      <Button variant={"outline"}>outline</Button>
+      <Button variant={"muted"}>muted</Button>
+      <Button variant={"teritary"}>teritary</Button>
+    </div>
+  );
 }
